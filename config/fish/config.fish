72085eced5f5fb5cb1a173766d408746354c58ba
shashamin chokjaroenwathanakul <shashah@interaction.co>
2026-05-09 11:59:47 -0700

    fish config with yazi

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,32 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    zoxide init fish | source
 end
-alias gs="git status"
-alias gp="git push"
-alias gc="git commit"
-alias gaa="git add ."
-alias ga="git add"
-alias gpl="git pull"
-alias vim nvim
-alias v nvim
-set autochdir
-zoxide init fish | source
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+
+    if test -s "$tmp"
+        set cwd (cat "$tmp")
+        if test -n "$cwd" -a "$cwd" != "$PWD"
+            cd "$cwd"
+        end
+    end
+
+    rm -f "$tmp"
+end
+# pnpm
+set -Ux PNPM_HOME /Users/shashaminchokjaroenwathanakul/Library/pnpm
+
+
+abbr gs "git status"
+abbr gp "git push"
+abbr gc "git commit"
+abbr gaa "git add ."
+abbr ga "git add"
+abbr gpl "git pull"
+abbr c "claude"
+abbr cds "claude --dangerously-skip-permissions"
+
+abbr v nvim
+abbr vim nvim
