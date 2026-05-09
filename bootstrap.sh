3f51259d3ca5b48d31656a5953aa342d890d6f3d
shashamin chokjaroenwathanakul <shashah@interaction.co>
2026-05-09 11:59:00 -0700

    cursor key bindings, navigation btw panels and quick chat

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,17 @@ ln -sfn $HOME/dotfiles/config/aerospace $HOME/.config/aerospace
 ln -sfn $HOME/dotfiles/config/sketchybar $HOME/.config/sketchybar
 ln -sfn $HOME/dotfiles/config/fish $HOME/.config/fish
 ln -sfn $HOME/dotfiles/config/nvim $HOME/.config/nvim
-ln -sfn $HOME/dotfiles/config/vscode $HOME/Library/Application\ Support/Code/User
+
+mkdir -p "$HOME/Library/Application Support/Code/User"
+mkdir -p "$HOME/Library/Application Support/Cursor/User"
+ln -sfn $HOME/dotfiles/config/vscode/settings.json    $HOME/Library/Application\ Support/Code/User/settings.json
+ln -sfn $HOME/dotfiles/config/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -sfn $HOME/dotfiles/config/cursor/settings.json    $HOME/Library/Application\ Support/Cursor/User/settings.json
+ln -sfn $HOME/dotfiles/config/cursor/keybindings.json $HOME/Library/Application\ Support/Cursor/User/keybindings.json
+
+mkdir -p $HOME/.config/zsh
 ln -sfn $HOME/dotfiles/config/zsh/.zprofile $HOME/.config/zsh/.zprofile
 ln -sfn $HOME/dotfiles/config/zsh/.zenv $HOME/.config/zsh/.zenv
+ln -sfn $HOME/dotfiles/config/.glzr $HOME/.glzr
 
 
